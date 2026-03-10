05275decfcbc6fd6305b32d8e78e51d0e31fa36e
Erick Martínez <3rick694@gmail.com>
2026-03-10 12:32:16 -0600

    fix: Change in the Cors option to allow multiple sources in the backend

--- src/main/java/com/managementsystem/managementsystembackend/Config/CorsConfig.java
+++ src/main/java/com/managementsystem/managementsystembackend/Config/CorsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
     @Value("${management-system.cors.allowed-origins}")
-    private String allowedOrigins;
+    private String[] allowedOrigins;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -17,7 +17,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(allowedOrigins)
+                        .allowedOriginPatterns(allowedOrigins)
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
