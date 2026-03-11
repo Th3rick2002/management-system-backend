eef2ac5d9d7fcc2eecf7d1fd414721a86f63839c
Erick Martínez <3rick694@gmail.com>
2026-03-11 14:43:24 -0600

    fix: changed allowedOrigins to String[] to String for unique origin

--- src/main/java/com/managementsystem/managementsystembackend/Config/CorsConfig.java
+++ src/main/java/com/managementsystem/managementsystembackend/Config/CorsConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class CorsConfig {
     @Value("${management-system.cors.allowed-origins}")
-    private String[] allowedOrigins;
+    private String allowedOrigins;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
